83d7519aac77675c838948454e6727290e43da31
Pedro Henrique <pedrosilva@ads.fiponline.edu.br>
2022-05-03 21:08:19 -0300

    teste aula

---
+++ app/src/main/java/com/example/vou_nao_vou_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.vou_nao_vou_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
