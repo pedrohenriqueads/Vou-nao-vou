86075171f222fb319a0b498ecf1d06eec1158002
Pedro Henrique <pedrosilva@ads.fiponline.edu.br>
2022-05-11 19:26:36 -0300

    Construcao da Home

---
+++ app/src/main/java/com/example/vou_nao_vou_app/MainActivityTelaHome.kt
@@ -0,0 +1,11 @@
+package com.example.vou_nao_vou_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityTelaHome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_tela_home)
+    }
+}
